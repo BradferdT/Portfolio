409f7be5bb41baa1ef799f1648db4622ef3bfd2e
Bradf3rd <Bradf3rdcc@hotmail.com>
2017-08-10 15:52:07 -0700

    added content to skills, animations for rockets resembling progress

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
     $('.main').css('display','block');
     $('.planet').css('display','block');
   }, 2000);
+
+  var options = [
+   {selector: '.header', offset: $(window).height() * 2, callback: function() {
+     $('.htmlProg').addClass('perfectSkillRow');
+     $('.htmlProgressHeight').addClass('shrinkRow');
+     $('.ngProg').addClass('perfectSkillRow');
+     $('.ngProgressHeight').addClass('shrinkRow');
+     $('.cssProg').addClass('perfectSkillRow');
+     $('.cssProgressHeight').addClass('shrinkRowLess');
+     $('.jsProg').addClass('perfectSkillRow');
+     $('.jsProgressHeight').addClass('shrinkRowLess');
+     $('.reactProg').addClass('perfectSkillRow');
+     $('.reactProgressHeight').addClass('shrinkRowMore');
+     $('.jqueryProg').addClass('perfectSkillRow');
+     $('.jqueryProgressHeight').addClass('shrinkRow');
+   } },
+ ];
+ Materialize.scrollFire(options);
+
+
 })
