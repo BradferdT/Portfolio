0ccb8327732e28410d6c3bb2f163d8ec3fd16a5d
Bradf3rd <Bradf3rdcc@hotmail.com>
2017-08-09 23:28:34 -0700

    small changes visually. reworked layout of html and css, going to be easier to add to.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+  $('.scrollspy').scrollSpy({
+    'scrollOffset': 0
+  });
   $('.spaceship').addClass('animateSpaceship');
   setTimeout(function () {
     $('.loading').fadeOut();
     $('.main').css('display','block');
+    $('.planet').css('display','block');
   }, 2000);
 })
